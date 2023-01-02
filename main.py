3cf5234751af01ff3f2c2d16db0e07d1a8b7f5b3
dvJoshr <joseph.64@outlook.com>
2023-01-02 18:53:55 -0500

    Add icon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 import model.calidad_del_aire as cal
@@ -15,6 +19,8 @@ class Enviroment(BaseModel):
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+parent_dir_path = os.path.dirname(os.path.realpath(__file__))
 posts = []
 
 enviroments = []
@@ -22,6 +28,12 @@ enviroments = []
 @app.get('/')
 def read_root():
     return {'Welcome': 'Welcome to my API server'}
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.ico"
+    print(parent_dir_path)
+    return FileResponse(path=parent_dir_path + "/static/" + file_name)
 
 @app.get('/posts')
 def get_posts():
